f28dbe6d19f161e4a9ec2400b1e87b42ffaa64d3
Ben Dixon <ben@hillsbede.co.uk>
2013-07-09 14:37:17 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "nginx-tlq"
+maintainer        "Ben Dixon"
+maintainer_email  "ben@hillsbede.co.uk"
+description       "Installs the nginx web server from the nginx ppa"
+version           "0.0.1"
+
+recipe "nginx-tlq", "nginx server"
+
+supports "ubuntu"
---
+++ recipes/default.rb
@@ -0,0 +1,46 @@
+# Add the stable nginx repo, stock ubuntu version
+# tends to be at least one major version behind
+
+bash 'adding stable nginx ppa' do
+  user 'root'
+  code <<-EOC
+    add-apt-repository ppa:nginx/stable
+    apt-get update
+  EOC
+end
+
+# install nginx
+package "nginx"
+
+# create the new main nginx config file
+# do as little as possible in here, most 
+# of this should be configured per site
+template "/etc/nginx/nginx.conf" do
+  owner "root"
+  group "root"
+  mode "0644"
+  source "nginx.conf.erb"
+end
+
+# monit pass through
+if @node[:monit_address]
+  template "/etc/nginx/sites-enabled/monit" do
+    owner "deploy"
+    group "deploy"
+    mode "0644"
+    source "monit_interface.erb"
+  end
+end
+
+# open port 80
+bash "allowing nginx traffic through firewall" do
+  user "root"
+  code "ufw allow 80 && ufw allow 443"
+end
+
+# restart nginx
+bash "restarting nginx" do
+  code <<-EOH
+    /etc/init.d/nginx restart
+  EOH
+end
