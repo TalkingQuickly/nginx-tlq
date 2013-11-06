b614ed2366f7cb3fe07f108fdf65d21af1076512
Ben Dixon <ben@hillsbede.co.uk>
2013-11-06 16:48:29 +0000

    increase hash bucket size

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nginx-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@hillsbede.co.uk"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.1"
+version           "0.0.2"
 
 recipe "nginx-tlq", "nginx server"
 
