863f0faadbc5c61e925cb7de10dba97fc695fa89
Ben Dixon <ben@hillsbede.co.uk>
2013-12-18 10:25:03 +0000

    Remove comment from above pid line in nginx config
    Address the issue described here:
    http://srced.com/2013/04/nginx-init-d-script-issue-watch-your-comments/

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nginx-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.3"
+version           "0.0.4"
 
 recipe "nginx-tlq", "nginx server"
 
